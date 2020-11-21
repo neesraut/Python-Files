c11a66556989b4d77fd9549b7b148e77b818af0a
neesraut <neesraut1@gmail.com>
2020-11-21 11:39:54 +1100

    loop

--- listAndDict.py
+++ listAndDict.py
@@ -1,22 +1,30 @@
+
+##list and dict combo - msot used in real world
 test_dict =[
             {
                 'name':'Ram',
-                'class':'5',
+                'classs':'5',
                 'country' : 'Nepal'
             },
 
             {
                 'name':'Hari',
-                'class':'6',
+                'classs':'6',
                 'country' : 'Australia'
 
             },
             {
                 'name':'Shyam',
-                'class':'8',
+                'classs':'8',
                 'country' : 'China'
             }
 ]
-print(test_dict)
+#print(test_dict)
+##print all the elements in list
+#for each in test_dict:
+   # print("Each element is{each}".format(each=each))
+
+##to print each elements in dictinory items within List
 for each in test_dict:
-    print("Each element is{each}".format(each=each))
+   print("Each Dict Item")    
+   print("Name is:{name}, class is :{classs}, country is: {country}\n".format(name=each['name'],classs=each['classs'],country=each['country']))
