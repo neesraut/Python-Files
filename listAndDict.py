7f6f1625376c9e6f8f1b195e365eb28588357810
neesraut <neesraut1@gmail.com>
2020-11-21 11:57:43 +1100

    loopWithFilteredData

--- listAndDict.py
+++ listAndDict.py
@@ -1,5 +1,6 @@
 
 ##list and dict combo - msot used in real world
+#for single line commenting use # and for bulk lines use '''
 test_dict =[
             {
                 'name':'Ram',
@@ -25,6 +26,23 @@ test_dict =[
    # print("Each element is{each}".format(each=each))
 
 ##to print each elements in dictinory items within List
-for each in test_dict:
+'''for each in test_dict:
    print("Each Dict Item")    
    print("Name is:{name}, class is :{classs}, country is: {country}\n".format(name=each['name'],classs=each['classs'],country=each['country']))
+'''
+#to print/access the items from dict within list that has particual data in it
+'''for each in test_dict:
+    if each['name']=='Ram':
+        print(each)
+'''
+
+##to print the data where there is multiple conditions
+'''for each in test_dict:
+    if each['name'] in ['Ram','Hari']:
+        print("The filtered elements are{each}".format(each=each))
+'''
+##to print the data where there is multiple conditions for different element
+'''for each in test_dict:
+    if each['name']=='Ram' and each['classs']=='5':
+        print("Data with name ram and class 5 is {each}".format(each=each))
+'''
