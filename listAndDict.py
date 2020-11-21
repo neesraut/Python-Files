dfbd89461c26aaf80730d0ab0d29388423373d65
neesraut <neesraut1@gmail.com>
2020-11-21 11:21:25 +1100

    forLoop

--- listAndDict.py
+++ listAndDict.py
@@ -1,1 +1,22 @@
-test_
+test_dict =[
+            {
+                'name':'Ram',
+                'class':'5',
+                'country' : 'Nepal'
+            },
+
+            {
+                'name':'Hari',
+                'class':'6',
+                'country' : 'Australia'
+
+            },
+            {
+                'name':'Shyam',
+                'class':'8',
+                'country' : 'China'
+            }
+]
+print(test_dict)
+for each in test_dict:
+    print("Each element is{each}".format(each=each))
